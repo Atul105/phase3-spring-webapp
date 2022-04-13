02f55a0c31f9b8714e506bcb447ffa5a7f213a7b
Atul105 <atulboby.magar1@gmail.com>
2022-04-13 18:28:27 +0000

    spring webapp

---
+++ src/main/java/com/simplilearn/webapp/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.simplilearn.webapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping(value="/",method=RequestMethod.GET)
+	@ResponseBody
+	public String StringindexMapping() {
+		return "The server is up and running";
+	}
+
+}
